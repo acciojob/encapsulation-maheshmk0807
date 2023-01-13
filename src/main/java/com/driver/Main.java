4b58b0607c38e40c82be8cea60a6897d58c45fe1
Mahesh Khedkar <maheshmk0807@gmail.com>
2023-01-14 00:09:13 +0530

    ans

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[])
+    {
+        RWOnly a = new RWOnly();
+        a.setX(5);
+        System.out.print(a.getX());
+    }
   
 }
